f51d44a64878157efe79e7cd4a591ccea808d7f2
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-06-23 19:05:48 +0530

    fixed offline notes submission

--- app/src/main/java/com/falcon/notesapp/NoteFragment.kt
+++ app/src/main/java/com/falcon/notesapp/NoteFragment.kt
@@ -131,6 +131,7 @@ class NoteFragment : Fragment() {
                     storeNoteInDatabase(noteRequest)
                 }
             }
+            findNavController().popBackStack()
         }
     }
 
